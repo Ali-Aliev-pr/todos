13562b01c46db848d851586314a7da40e26ba21f
Ali-Aliev-pr <72982603+Ali-Aliev-pr@users.noreply.github.com>
2022-02-25 18:15:52 +0300

    netlif

--- src/components/customHooks/useFetch.js
+++ src/components/customHooks/useFetch.js
@@ -9,7 +9,7 @@ const useFetch = (url) => {
             headers: {'Accept': 'application/json'}
         })
         .then(res => res.json())
-        .then(e => e.map(e => console.log(e)))
+        .then(e => setData(e))
       }, [])
 
     return (
